b080eb952edad49dda5ece0b3bc2707669ef4cca
FinnT730 <FinnT730@gmail.com>
2020-07-08 17:07:07 +0200

    Init

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "ScalaDevJS_diffFinder"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
+
+
+enablePlugins(ScalaJSPlugin)
+scalaJSUseMainModuleInitializer := true
---
+++ src/main/scala/cf/finnt730/Main.scala
@@ -0,0 +1,18 @@
+package cf.finnt730
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    def indexOfArgs = 0
+    args.foreach((a) => {
+      def result = a match {
+        case "-run" => "Command =\t"
+        case "HELLO_WORLD" => "------"
+      }
+      println(result)
+      indexOfArgs.+(1);
+    })
+  }
+
+
+}
